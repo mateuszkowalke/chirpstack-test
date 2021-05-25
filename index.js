b6571a8f88b9a3429252910a62dc7bf019f3fc93
mateusz.kowalke <mateusz.kowalke@aurafutures.com>
2021-05-25 09:48:20 +0200

    fix

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  return res.json({ result: "success" });
+});
+
 app.post("/", (req, res) => {
   console.log(req.body);
   return res.json({ response: "ok" });
